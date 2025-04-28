f76b523d35d26948e556ea8b237090bdd5d9228b
DontCallMeLuca <lgoddijn@student.codam.nl>
2025-04-28 12:24:27 +0200

    Forgor to save a file

--- src/init.c
+++ src/init.c
@@ -8,6 +8,20 @@
 
 #include "internal_headers/lgmalloc_global_include.h"
 
+/* Global initializer flag, initializes on first call of api */
+
+static int __is_lgmalloc_init_g = 0;
+
+static ALWAYS_INLINE void lgmalloc_set_init(int value)
+{
+	__is_lgmalloc_init_g = !!value;
+}
+
+static ALWAYS_INLINE int lgmalloc_is_init(void)
+{
+	return __is_lgmalloc_init_g;
+}
+
 /* For scope and responsibility reasons this is
  * the only function that handles the init flags.
  * Either the caller handles the flag or the callee. */
